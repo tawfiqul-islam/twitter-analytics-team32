1e48a470414302fb4e9d9f9c880b26a717df12da
Tawfiqul Islam <muhammedi@student.unimelb.edu.au>
2017-04-17 13:13:16 +1000

    2nd

--- ClusterManager/src/core/setup.py
+++ ClusterManager/src/core/setup.py
@@ -6,6 +6,7 @@ from boto.ec2.regioninfo import RegionInfo
 
 def connect():
     
+    
     region=RegionInfo(name='melbourne', endpoint='nova.rc.nectar.org.au')
     
     ec2_conn = boto.connect_ec2(aws_access_key_id='741b95cd6ff9487c93feddbdf0dbdf8a',
