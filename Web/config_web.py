4f57ebe790d7ac00bb7725f32c4e1355101e227d
Stephen Tjandra <stephen_tjandra@hotmail.com>
2017-04-29 18:07:13 +1000

    LGA has method needed by harvester.
    Create a view for geojson. Code to store selected values of geojson to couchdb. A method in class LGA that returns the approximate centre of each LGA area and approximate radius in km. LGA class now reads from couchdb

--- Web/config_web.py
+++ Web/config_web.py
@@ -4,6 +4,14 @@ import configparser
 
 DATA_PATH = './static/resources/data/'
 
+VIEW_GEOJSON_MAP = \
+    '''function (doc) {
+        if (doc.doc_type == 'lga_geojson_feature') {
+            emit(doc.lga_code, doc.columns);
+            }
+        }
+    '''
+
 
 def main():
     if (len(sys.argv) != 2):
@@ -17,9 +25,13 @@ def main():
     config['couchdb'] = {'port': '5984',
                          'ip_address': ip_address,
                          'db_name_aurin': 'aurin',
-                         'key': 'lga_code'}
+                         'key': 'lga_code',
+                         'view_geojson': {'docid': '_design/lga',
+                                          'view_name': 'features-view',
+                                          'map_func': VIEW_GEOJSON_MAP}}
 
-    config['geojson_file'] = {'lga': DATA_PATH + 'vic-lga.json'}
+    config['geojson_file'] = {'lga': DATA_PATH + 'vic-lga.json',
+                              'doc_type': 'lga_geojson_feature'}
 
     config['aurin_json_files'] = {'internet_access': DATA_PATH + 'LGA11_Internet_Access_at_Home.json',
                                   'sitting_hours': DATA_PATH + 'LGA_Sedentary_behaviour__sitting_hours_per_day_.json',
